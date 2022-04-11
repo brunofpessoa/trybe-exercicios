e7a9fc2e5370fd0fdc27a7337a6f1b94ae4b57a7
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-04-11 15:49:13 -0300

    Corrige sintaxe

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/exercicios-fixacao/script.js
@@ -0,0 +1,24 @@
+// Requisito 1
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3
+    }
+}
+
+// Requisito 2
+console.log(`A jogadora ${player.name + " " + player.lastName} tem ${player.age} anos de idade`);
+
+// Requisito 3
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+// Requisito 4
+const fullName = player.name + " " + player.lastName;
+const manyTimesBest = player.bestInTheWorld.length;
+console.log(`A jogadora ${fullName} foi eleita a melhor do mundo por ${manyTimesBest} vezes`);
+
+// Requisito 5
+console.log(`A jogadora possui ${player.medals.golden} medalhas de ouro e ${player.medals.silver} medalhas de prata`);
