3744b1b8d82c25a1d0ccc9706e7a87ad0ea796fd
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-04-14 10:10:09 -0300

    Adiciona exercicio  bonus 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/bonus/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/bonus/script.js
@@ -92,3 +92,36 @@ function generateFruitBasket(basket){
     console.log(resultToPrint.slice(0 , resultToPrint.length - 2));
 }
 generateFruitBasket(basket);
+
+// Exercício 4 a = { [ {}, {} ] }
+let moradores = {
+    blocoUm: [
+      {
+        nome: 'Luiza',
+        sobrenome: 'Guimarães',
+        andar: 10,
+        apartamento: 1005,
+      },
+      {
+        nome: 'William',
+        sobrenome: 'Albuquerque',
+        andar: 5,
+        apartamento: 502,
+      },
+    ],
+    blocoDois: [
+      {
+        nome: 'Murilo',
+        sobrenome: 'Ferraz',
+        andar: 8,
+        apartamento: 804,
+      },
+      {
+        nome: 'Zoey',
+        sobrenome: 'Brooks',
+        andar: 1,
+        apartamento: 101,
+      },
+    ],
+  };
+console.log(`O morador do bloco 2 de nome ${moradores['blocoDois'][1].nome} ${moradores['blocoDois'][1].sobrenome} mora no ${moradores['blocoDois'][1].andar}° andar, apartamento ${moradores['blocoDois'][1].apartamento}`);
