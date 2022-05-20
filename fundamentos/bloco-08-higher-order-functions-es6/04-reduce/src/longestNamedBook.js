3c5246629b36e5e2f85f71251242e736e122c5ab
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-05-19 21:33:46 -0300

    Adiciona exercícios 4 - longestNamedBook.js

--- fundamentos/bloco-08-higher-order-functions-es6/04-reduce/src/longestNamedBook.js
+++ fundamentos/bloco-08-higher-order-functions-es6/04-reduce/src/longestNamedBook.js
@@ -1,14 +1,26 @@
+const books = require("./data.js");
+
 const expectedResult = {
   id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  genre: 'Fantasia',
+  name: "As Crônicas de Gelo e Fogo",
+  genre: "Fantasia",
   author: {
-    name: 'George R. R. Martin',
+    name: "George R. R. Martin",
     birthYear: 1948,
   },
   releaseYear: 1991,
 };
 
-function longestNamedBook() {
-  // escreva seu código aqui
+// This don't seems a good use for reduce method, it's not looks like a conventional use of reduce
+// But is what the statement asks to do
+function longestNamedBook(data) {
+  const longestName = data.reduce((acc, curr) => {
+    if (curr.name.length > acc.name.length) {
+      acc = curr;
+    }
+    return acc;
+  });
+  return longestName;
 }
+
+console.log(longestNamedBook(books));
