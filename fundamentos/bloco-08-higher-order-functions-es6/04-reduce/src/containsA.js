4ff75f61c4b0a4ba32fa6e44a4dccc99eeeba23b
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-05-19 21:44:43 -0300

    Adiciona exercício 6 - containsA.js

--- fundamentos/bloco-08-higher-order-functions-es6/04-reduce/src/containsA.js
+++ fundamentos/bloco-08-higher-order-functions-es6/04-reduce/src/containsA.js
@@ -1,9 +1,17 @@
 const names = [
-  'Aanemarie', 'Adervandes', 'Akifusa',
-  'Abegildo', 'Adicellia', 'Aladonata',
-  'Abeladerco', 'Adieidy', 'Alarucha',
+  "Aanemarie",
+  "Adervandes",
+  "Akifusa",
+  "Abegildo",
+  "Adicellia",
+  "Aladonata",
+  "Abeladerco",
+  "Adieidy",
+  "Alarucha",
 ];
 
-function containsA() {
-  // escreva seu código aqui
+function containsA(data) {
+  return data.reduce((acc, curr) => (curr.toLowerCase().startsWith("a") ? (acc += 1) : acc), 0);
 }
+
+console.log(containsA(names));
