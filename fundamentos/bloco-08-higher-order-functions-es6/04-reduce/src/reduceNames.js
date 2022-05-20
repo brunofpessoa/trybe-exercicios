7705d3459764e9a8e59e90217e191ba938ff6875
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-05-19 21:02:40 -0300

    Remove espaço no início da string retornada.

--- fundamentos/bloco-08-higher-order-functions-es6/04-reduce/src/reduceNames.js
+++ fundamentos/bloco-08-higher-order-functions-es6/04-reduce/src/reduceNames.js
@@ -4,13 +4,15 @@ const expectedResult =
   "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
 function reduceNames(data) {
-  return data.reduce(
-    (acc, curr, index, arr) =>
-      index === arr.length - 1
-        ? `${acc} ${curr.author.name}.`
-        : `${acc} ${curr.author.name},`,
-    ""
-  );
+  return data
+    .reduce(
+      (acc, curr, index, arr) =>
+        index === arr.length - 1
+          ? `${acc} ${curr.author.name}.`
+          : `${acc} ${curr.author.name},`,
+      ""
+    )
+    .trim();
 }
 
 console.log(reduceNames(books));
